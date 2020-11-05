5cbc1c0787d35c718d4533302424b11c776ce75d
fermartinotti <fermartinotti@gmail.com>
2020-11-05 09:30:06 -0300

    se agrega avatar

--- lib/src/pages/alert_page.dart
+++ lib/src/pages/alert_page.dart
@@ -10,6 +10,16 @@ class AlertPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Alert Page'),
       ),
+      body: Center(
+        child: RaisedButton(
+          child: Text('Mostrar Alerta'),
+          color: Colors.blue,
+          textColor: Colors.white,
+          shape: StadiumBorder(),
+          onPressed: () => _mostrarAlerta(context),
+          
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.arrow_back),
         onPressed: (){
@@ -19,4 +29,38 @@ class AlertPage extends StatelessWidget {
       ),
     );
   }
+
+  void _mostrarAlerta(BuildContext context){
+
+    showDialog(
+      context: context,
+      barrierDismissible: true,
+      builder: (context) {
+        return AlertDialog(
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+          title: Text('Titulo'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text ('contenido'),
+              FlutterLogo(size: 30)
+            ],
+          ),
+          actions: [
+            FlatButton(
+              onPressed: () => Navigator.of(context).pop(), 
+              child: Text('Cancelar')
+            ),
+            FlatButton(
+              onPressed: (){
+                Navigator.of(context).pop();
+              }, 
+              child: Text('Ok')
+            )
+          ],
+        );
+      }
+    );
+  }
+
 }
--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -9,6 +9,28 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Avatar Page'),
+        actions: [
+          Container(
+            margin: EdgeInsets.only(right: 10),
+            child: CircleAvatar( 
+              child: Text('SM'),
+              backgroundColor: Colors.brown,
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(8),
+            child: CircleAvatar(
+              radius: 20, 
+              backgroundImage: NetworkImage('https://imagenes.20minutos.es/files/image_656_370/uploads/imagenes/2005/11/25/250713.jpg'),
+            ),
+          )
+        ],
+      ),
+      body: Center( 
+        child: FadeInImage(
+          placeholder: AssetImage('assets/jar-loading.gif'),
+          image: NetworkImage('https://imagenes.20minutos.es/files/image_656_370/uploads/imagenes/2005/11/25/250713.jpg')
+        ),
       ),
     );
   }
